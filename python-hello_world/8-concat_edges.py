3006993ece90be1ebb5f89d90994afd5de33b7c2
hotay11 <j.hotay@alustudent.com>
2026-06-18 16:27:29 -0700

    Create a new sentence

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Complete this source code to print object-oriented programming with Python, followed by a new line.
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[106:112] + str[:6])
+
