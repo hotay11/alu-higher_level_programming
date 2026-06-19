1485ab49ab8fb0b12762d2335011acae6e0965cb
hotay11 <j.hotay@alustudent.com>
2026-06-19 15:51:28 -0700

    rectifications

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i) + "{:02d}".format(j), end="")
+        print("{}{}".format(i, j), end="")
         print("" if i == 8 and j == 9 else ", ", end="")
 print("")
