31cdbc02bc3790a4bf636aa47d999252a94caed7
hotay11 <j.hotay@alustudent.com>
2026-06-21 15:17:46 -0700

    To upper case

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that prints a string in uppercase."""
+
+
+def uppercase(str):
+    """Print a string in uppercase, followed by a new line."""
+    result = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))
