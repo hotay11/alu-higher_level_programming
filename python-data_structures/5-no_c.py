415115ceee79f04bff8ff7840e0ef7c903202b4f
hotay11 <j.hotay@alustudent.com>
2026-07-01 14:59:42 -0700

    Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-    if idx < 0 or idx >= len(new_list):
-        return new_list
-    new_list[idx] = element
-    return new_list
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
