775a3c7160365dfd525ee5c682d8bf0fa27db819
hotay11 <j.hotay@alustudent.com>
2026-07-17 15:47:58 -0700

    My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module defines an empty class Square that represents a square."""
+
+
+class Square:
+    """Represents a square with no attributes or methods defined yet."""
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+0-square.py: Write an empty class Square that defines a square:
