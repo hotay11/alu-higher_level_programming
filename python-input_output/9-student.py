63e959e745aff6d7aa6d43dff2e7345978fa9c89
hotay11 <j.hotay@alustudent.com>
2026-07-29 02:14:09 +0300

    Create 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """A Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves dictionary representation of Student"""
+        return self.__dict__
+      
