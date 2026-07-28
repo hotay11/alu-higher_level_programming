4c6a9901ffba9dc0b7938e1a67ca1f111c2c9174
hotay11 <j.hotay@alustudent.com>
2026-07-29 02:21:37 +0300

    Create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """A Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation with optional filter"""
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)
