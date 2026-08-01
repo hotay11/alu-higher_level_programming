914f80bc9541b8904f7887cf3d6b0bae455bf6bc
hotay11 <j.hotay@alustudent.com>
2026-08-01 10:12:51 +0300

    Create 14-my_genres.sql

---
+++ 14-my_genres.sql
@@ -0,0 +1,8 @@
+-- lists all genres of the show Dexter
+-- the database name is passed as an argument of the mysql command
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
