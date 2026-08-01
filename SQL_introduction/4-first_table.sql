25399aba7c37c69f1c3d4cc7b1e7b2be0d54c1c9
hotay11 <j.hotay@alustudent.com>
2026-08-01 09:21:09 +0300

    Create 4-first_table.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- creates the table first_table in the current database
+-- the database name is passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
