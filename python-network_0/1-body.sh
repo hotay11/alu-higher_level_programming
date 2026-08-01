1d762a608e04d0599ade616c19c953160f799001
hotay11 <j.hotay@alustudent.com>
2026-08-01 10:58:52 +0300

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the body of a 200 status GET response, following redirects
-[ "$(curl -s -o /dev/null -w "%{http_code}" -L "$1")" = "200" ] && curl -s -L "$1"
+# Sends a GET request to the URL, follows redirects and displays the body of a 200 response
+curl -sfL "$1"
