751040f16fde2ae20ae641424066d7b8871957b5
hotay11 <j.hotay@alustudent.com>
2026-08-01 10:46:46 +0300

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- creates the MySQL server user user_0d_1 with all privileges
+-- Creates the user user_0d_1 with all privileges on the server
+-- Does not fail if the user already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grants every privilege on every database and table
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
