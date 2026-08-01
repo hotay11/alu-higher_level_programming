d54162c05e5e66b011d8f604869e4b3bdc99d649
hotay11 <j.hotay@alustudent.com>
2026-08-01 09:24:12 +0300

    Create 10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of second_table, ordered by score (top first)
+-- the database name is passed as an argument of the mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;
