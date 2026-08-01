a45d1c911cba45bd8c45f5745d952ad351a06235
hotay11 <j.hotay@alustudent.com>
2026-08-01 09:28:01 +0300

    Create 12-no_cheating.sql

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- updates the score of Bob to 10, using only the name field
+-- the database name is passed as an argument of the mysql command
+UPDATE second_table SET score = 10 WHERE name = "Bob";
