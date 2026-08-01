2a1a909a7e8328adeda91a9f4c800f33095e3989
hotay11 <j.hotay@alustudent.com>
2026-08-01 10:03:09 +0300

    Delete 4-never_empty.sql

--- 4-never_empty.sql
+++
@@ -1,6 +0,0 @@
--- creates the table id_not_null
--- the database name is passed as an argument of the mysql command
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256)
-);
