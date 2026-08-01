98c3548feacf3f22a352f1cab05ae27001b46b67
hotay11 <j.hotay@alustudent.com>
2026-08-01 18:33:34 +0300

    Create 2-post_email.py

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Send a POST request with an email parameter and display the response body."""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
