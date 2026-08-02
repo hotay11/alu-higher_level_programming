244fce0f6fdf675f9a399885941950164baeb497
hotay11 <j.hotay@alustudent.com>
2026-08-02 15:52:06 +0300

    Update 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request with a custom user-id header and displays the body
-curl -s -H "X-HolbertonSchool-User-Id: ${2:-98}" "$1"
+# Sends a GET request to a URL with the X-HolbertonSchool-User-Id header set to 98
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
